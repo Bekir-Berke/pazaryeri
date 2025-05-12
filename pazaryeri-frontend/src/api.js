4bd0282c69403fcaa639ea22923c51dfe6c90fbe
Bekir-Berke <76753313+Bekir-Berke@users.noreply.github.com>
2025-05-12 17:41:58 +0300

    url düzeltildi

--- pazaryeri-frontend/src/api.js
+++ pazaryeri-frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import router from "./router";
 
 const apiClient = axios.create({
-  baseURL:"http://localhost:3000/api",
+  baseURL:"https://pazaryeri.bekirberke.tr/api",
   headers: {
     "Content-Type": "application/json",
   },
